ee3fb0766f03087d5041b058a092e1169994526d
mercy021 <kinyanjuimercywangari@gmail.com>
2021-04-04 18:04:01 +0300

    first commit

---
+++ forin.js
@@ -0,0 +1,18 @@
+ var guitar=["Acoustic","Semi-acoustic","Classic","Electric"];
+
+ var text="";
+ var z;
+ for(z in guitar){
+     text +=guitar[z];
+ }
+ console.log(guitar);
+
+ let chocolates=["milk choco","dark choco","white choco","bittersweet"]
+
+ let y="";
+ 
+ for(let y of chocolates){
+     text +=y+"<br>";
+ }
+ console.log(chocolates);
+
